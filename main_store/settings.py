83c03a27ed4f1a8e2fb0bef94ac614fd523611f4
Rafael A. Flores M <rflores@activeit.cl>
2023-04-11 20:38:28 -0400

    upd: change order static url in setting

--- main_store/settings.py
+++ main_store/settings.py
@@ -126,11 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_URL = [
-    os.path.join(BASE_DIR,'rent_car','static')
-]
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'rent_car','static')
-]
 STATIC_ROOT = os.path.join(BASE_DIR,'rent_car','static')
+STATICFILES_URL = [ os.path.join(BASE_DIR,'rent_car','static') ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'rent_car','static') ]
